277ff00d1a58712e784ac03e4c30245380e3dd79
terence-lee-xin-jin <89919135+lee-xin-jin-terence@users.noreply.github.com>
2025-06-01 10:12:45 +0800

    Create average_marks_per_subject.sql

---
+++ sql/sqlite/queries/average_marks_per_subject.sql
@@ -0,0 +1,15 @@
+-- Query: Average marks per subject
+-- Description: Calculates the average marks obtained by students in each subject, rounded to 2 decimal places, and lists them in descending order.
+-- Use Case: Useful for identifying subjects with higher or lower student performance.
+
+SELECT 
+    sub.subjectTitle as subjectTitle,
+    ROUND(AVG(sr.marks), 2) AS averageMarks
+FROM 
+    SubjectResult sr
+JOIN
+    Subject sub ON sr.subjectId = sub.subjectId
+GROUP BY 
+    sub.subjectTitle
+ORDER BY
+    averageMarks DESC;
